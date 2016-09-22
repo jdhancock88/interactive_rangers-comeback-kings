8c5d0000a57c977aa2ab59bd5d721f8e7b24cce2
jdhancock88 <jdhancock@gmail.com>
2016-09-22 16:57:40 -0500

    Adds in slider. Still needs percentage readout. Consolidates simulation code. Updates standings table. Adds in win/loss graphic readout.

--- build/static/js/+custom.js
+++ build/static/js/+custom.js
@@ -5,40 +5,18 @@ $(document).ready(function() {
 	// injecting current year into footer
 	// DO NOT DELETE
 
+	var simData;
+
 	var d = new Date();
 	var year = d.getFullYear();
 
 	$('.copyright').text(year);
 
+	var sliderValue = 50;
 
-	var standings = [
-		{
-			"team": "Rangers",
-			"wins": 88,
-			"losses": 62
-		},
-		{
-			"team": "Mariners",
-			"wins": 79,
-			"losses": 70
-		},
-		{
-			"team": "Astros",
-			"wins": 78,
-			"losses": 71
-		},
-		{
-			"team": "Athletics",
-			"wins": 66,
-			"losses": 83
-		},
-		{
-			"team": "Angels",
-			"wins": 65,
-			"losses": 84
-		}
-	];
-
+	////////////////////////////////////////////////////////////////////////////
+	///// INITIAL DATA DRAW AND FORMATTING /////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
 
 	$.getJSON("/assets/python-scripts/json/rangers-game-logs.json", function(data) {
 
@@ -55,10 +33,11 @@ $(document).ready(function() {
 			}
 		});
 
+		simData = data;
+
 		drawGameLogs(data, "#tex-game-log-chart");
 		drawWinsLosses(texWL, "#tex-wins-losses");
 
-		simFormat(data);
 
 	});
 
@@ -83,6 +62,11 @@ $(document).ready(function() {
 
 	});
 
+
+	////////////////////////////////////////////////////////////////////////////
+	///// DRAWING THE GAME LOGS ////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
 	function drawGameLogs(data, target) {
 		d3.select(target).html("");
 
@@ -102,6 +86,10 @@ $(document).ready(function() {
 				});
 	}
 
+	////////////////////////////////////////////////////////////////////////////
+	///// DRAWING THE ONE-RUNS WINS AND LOSSES /////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
 	function drawWinsLosses(data, target) {
 
 		$(target).empty();
@@ -118,108 +106,182 @@ $(document).ready(function() {
 
 
 
+	////////////////////////////////////////////////////////////////////////////
+	///// RUNNING THE SIMULATION ///////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+
+	// ****** CLICKING THE SIM BUTTON ******
+
+	$("#simButton").click(function() {
+		console.log("test");
+
+		// resets standings to actual finish
+		var standings = [
+			{"team": "rangers","abbr": "TEX","wins": 88,"losses": 62},
+			{"team": "mariners","abbr": "SEA","wins": 79,"losses": 70},
+			{"team": "astros","abbr": "HOU","wins": 78,"losses": 71},
+			{"team": "athletics","abbr": "OAK","wins": 66,"losses": 83},
+			{"team": "angels","abbr": "LAA","wins": 65,"losses": 84}
+		];
+
+		$("#simButton").hide();
+		// hands off the standigs, gamelogs, and threshold to the simulation
+		runSim(standings, simData, sliderValue);
+	});
 
 
+	// ****** FORMATTING THE DATA ******
 
+	function runSim(standings, simData, rate) {
 
+		$("#simChart .chartBody").html("");
 
-
-
-	function simFormat(data) {
-
+		// clear our oneRunners, opponent and teamIndex variables
 		var oneRunners = [];
-		var adjustedStandings = standings;
-
 		var opponent, teamIndex;
 
-		$.each(data, function(k,v) {
+
+		// iterate over the original gamelogs to find if the rangers won/loss a one-run
+		// game. if so, update the standings by removing the win/loss from the rangers
+		// record. if it was a divisional opponent, also remove their win/loss
+		// we also populate the oneRunners array with those games, which we'll use to run the sim
+
+		$.each(simData, function(k,v) {
 			if (v.one_run_win === true) {
 				oneRunners.push(v);
-				adjustedStandings[0].wins--;
 
+				//update the rangers win total
+				standings[0].wins--; //updating the rangers win total
+
+				// find the opponent
 				opponent = v.opponent;
 
-				teamIndex = _.findIndex(adjustedStandings, function(o) {return o.team == opponent;});
-
+				// checking if the opponent is in the division. if so, update their loss total
+				teamIndex = _.findIndex(standings, function(o) {return o.team == opponent;});
 				if (teamIndex !== -1) {
-					adjustedStandings[teamIndex].losses--;
+					standings[teamIndex].losses--;
 				}
-
 			}
 
 			if (v.one_run_loss === true) {
 				oneRunners.push(v);
-				adjustedStandings[0].losses--;
 
+				//update the rangers loss total
+				standings[0].losses--;
+
+				// find opponent
 				opponent = v.opponent;
 
-				teamIndex = _.findIndex(adjustedStandings, function(o) {return o.team == opponent;});
-
+				// check if the opponent is a divisional oppennt, and if so, update their win total
+				teamIndex = _.findIndex(standings, function(o) {return o.team == opponent;});
 				if (teamIndex !== -1) {
-					adjustedStandings[teamIndex].wins--;
+					standings[teamIndex].wins--;
 				}
 			}
 		});
 
 
+		// ****** RUNNING THE SIMULATION ******
 
-		runSim(oneRunners, adjustedStandings, 50);
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	function runSim(data, standings, rate) {
-
-		console.log(standings);
+		// reset the rate to be a decimal
 		rate = rate / 100;
 
-		console.log(rate);
-		var opponent, teamIndex;
+		// clearing our opponent and teamIndex variables for reuse in the simulation
+		opponent = "";
+		teamIndex = 0;
 
-		$.each(data, function(k,v) {
+		var winsLosses = [];
+
+		// go through the games in oneRunners and decide a fictional outcome based on the win rate supplied
+		$.each(oneRunners, function(k,v) {
 
 			var currentOutcome;
 
+			// grab a random number, and see if it's less than (a win) or greater than (a loss)
+			// our supplied win rate
 			var n = Math.random();
+
+			// if the random number is less than our win rate, that counts as a win
+			// so, we update the rangers win total, check if the opponent is a divisional
+			// opponent, and if so, update their loss total in the standings
 			if (n <= rate) {
+				winsLosses.push("win");
 				standings[0].wins++;
-
 				opponent = v.opponent;
-				teamIndex = _.findIndex(standings, function(o) {return o.team == opponent;});
-
+				teamIndex = _.findIndex(standings, function(o) {return o.abbr == opponent;});
 				if (teamIndex !== -1) {
-					adjustedStandings[teamIndex].losses++;
+					standings[teamIndex].losses++;
 				}
 
+				// $("#simChart").append("<span class='game-log one-run-win'></span>");
+
+			// else, if the random number is greater than the win rate, it counts
+			// as a loss, so we update the rangers loss total in the standings, check
+			// if the opponent is a divisonal opponent, and if so, update their win total
 			} else {
+				winsLosses.push("loss");
 				standings[0].losses++;
-
 				opponent = v.opponent;
-				teamIndex = _.findIndex(standings, function(o) {return o.team == opponent;});
-
+				teamIndex = _.findIndex(standings, function(o) {return o.abbr == opponent;});
 				if (teamIndex !== -1) {
-					adjustedStandings[teamIndex].wins++;
+					standings[teamIndex].wins++;
 				}
+
+				// $("#simChart").append("<span class='game-log one-run-loss'></span>");
 			}
 
 		});
 
-		console.log(standings);
+		for (i=0; i<winsLosses.length; i++) {
+
+			setTimeout(function(x) {
+				return function() {
+					if (winsLosses[x] === "win") {
+						$("#simChart").append("<span class='game-log one-run-win'></span>");
+					} else {
+						$("#simChart").append("<span class='game-log one-run-loss'></span>");
+					}
+
+					if (x === winsLosses.length - 1) {
+						// now that our standings have been simulated, we want to redorder the
+						// array by team wins
+						standings = _.orderBy(standings, "wins", "desc");
+
+						// set a variable equal to the number of wins of the highest team
+						var leadWins = standings[0].wins;
+
+						// go through the standings again, figuring out how how many games behind
+						// each team is from the first-place team, and each team's win percentage
+						// then update the html standings table with the new wins, losses, win perct.,
+						// and games behind totals
+						$.each(standings, function(k,v) {
+							var gamesBack = leadWins - v.wins;
+							console.log(k);
+							v.games_behind = gamesBack;
+							v.pct = (v.wins / (v.wins + v.losses)).toFixed(3).slice(1,5);
+
+							$("#" + v.team + "-row").children("td").eq(1).text(v.wins);
+							$("#" + v.team + "-row").children("td").eq(2).text(v.losses);
+							$("#" + v.team + "-row").children("td").eq(3).text(v.pct);
+							$("#" + v.team + "-row").children("td").eq(4).text(v.games_behind);
+
+							// each table row is absolutely positioned within the table so we can
+							// animate the change in position after the simulation runs. We know each
+							// table row is 29px tall, so we just multiply that by the key of that particular
+							// team in the standings array.
+							$("#" + v.team + "-row").css("top", "calc(29px * " + (k + 1) + ")");
+
+						});
+					}
+				};
+			}(i), 200*i);
+		}
 	}
 
+
+	////////////////////////////////////////////////////////////////////////////
+	///// CHANGING THE OPPONENT FOR THE GAME LOG CHART /////////////////////////
+	////////////////////////////////////////////////////////////////////////////
 
 	$("#opponents-selector select").change(function() {
 		var selectedTeam = $(this).children("option:selected").attr("data-team");
@@ -248,6 +310,18 @@ $(document).ready(function() {
 
 		});
 
+	});
+
+
+	$("#slider").slider({
+		animate: "fast",
+		max: 100,
+		value: 50,
+		change: function() {
+			console.log("test");
+			sliderValue = $("#slider").slider("value");
+			console.log($("#slider").slider("value"));
+		}
 	});
 
 
